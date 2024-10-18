933e361bd116f703834d36882f7646f13e289d29
João Staub <joao.staub42@gmail.com>
2024-10-18 14:59:14 -0300

    delete limpando o repo

--- ler_url.py
+++
@@ -1,6 +0,0 @@
-import pandas
-
-# read dataset/dataset_filtrado.csv
-df = pandas.read_csv("./dataset/dataset_filtrado.csv", sep=";")  # Example for semicolon
-
-print(df["URL"])  # Print the column "url"
